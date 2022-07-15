9008b628011de96c087b62a5cbaa72916f14e496
marius-lionte <mariusmihai.learning@gmail.com>
2022-07-15 21:22:03 +0300

    ci: setup gitops project & Jenkinsfile

---
+++ gitops/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, send_from_directory
+import os
+
+app = Flask(__name__)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico')
+
+@app.route('/')
+def hello_world():
+    return 'Flask app!!!'
